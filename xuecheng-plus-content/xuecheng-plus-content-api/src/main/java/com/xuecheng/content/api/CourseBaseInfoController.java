9f8fb37e8ecb9f40882e76b54a9ba145f195830b
code-rabbits <njq3699@gmail.com>
2023-01-14 16:05:06 +0800

    Day3-异常处理和JSR303校验

--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
@@ -1,5 +1,6 @@
 package com.xuecheng.content.api;
 
+import com.xuecheng.base.exception.ValidationGroups;
 import com.xuecheng.base.model.PageParams;
 import com.xuecheng.base.model.PageResult;
 import com.xuecheng.content.model.dto.AddCourseDto;
@@ -9,6 +10,7 @@ import com.xuecheng.content.model.po.CourseBase;
 import com.xuecheng.content.service.CourseBaseInfoService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,7 +42,7 @@ public class CourseBaseInfoController {
 
     @ApiOperation("新增课程")
     @PostMapping("/course")
-    public CourseBaseInfoDto createCourseBase(@RequestBody AddCourseDto addCourseDto){
+    public CourseBaseInfoDto createCourseBase(@RequestBody @Validated(ValidationGroups.Inster.class) AddCourseDto addCourseDto){
 
         //获取当前用户所属培训机构的id
         Long companyId = 22L;
@@ -49,5 +51,11 @@ public class CourseBaseInfoController {
         CourseBaseInfoDto courseBase = courseBaseInfoService.createCourseBase(companyId, addCourseDto);
         return courseBase;
     }
+
+
+
+
+
+
 
 }
--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/impl/CourseBaseInfoServiceImpl.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/impl/CourseBaseInfoServiceImpl.java
@@ -92,33 +92,33 @@ public class CourseBaseInfoServiceImpl implements CourseBaseInfoService {
     public CourseBaseInfoDto createCourseBase(Long companyId, AddCourseDto dto) {
         //对参数进行合法性的校验
         //合法性校验
-        if (StringUtils.isBlank(dto.getName())) {
-            throw new RuntimeException("课程名称为空");
-        }
-
-        if (StringUtils.isBlank(dto.getMt())) {
-            throw new RuntimeException("课程分类为空");
-        }
-
-        if (StringUtils.isBlank(dto.getSt())) {
-            throw new RuntimeException("课程分类为空");
-        }
-
-        if (StringUtils.isBlank(dto.getGrade())) {
-            throw new RuntimeException("课程等级为空");
-        }
-
-        if (StringUtils.isBlank(dto.getTeachmode())) {
-            throw new RuntimeException("教育模式为空");
-        }
-
-        if (StringUtils.isBlank(dto.getUsers())) {
-            throw new RuntimeException("适应人群为空");
-        }
-
-        if (StringUtils.isBlank(dto.getCharge())) {
-            throw new RuntimeException("收费规则为空");
-        }
+        // if (StringUtils.isBlank(dto.getName())) {
+        //     throw new RuntimeException("课程名称为空");
+        // }
+        //
+        // if (StringUtils.isBlank(dto.getMt())) {
+        //     throw new RuntimeException("课程分类为空");
+        // }
+        //
+        // if (StringUtils.isBlank(dto.getSt())) {
+        //     throw new RuntimeException("课程分类为空");
+        // }
+        //
+        // if (StringUtils.isBlank(dto.getGrade())) {
+        //     throw new RuntimeException("课程等级为空");
+        // }
+        //
+        // if (StringUtils.isBlank(dto.getTeachmode())) {
+        //     throw new RuntimeException("教育模式为空");
+        // }
+        //
+        // if (StringUtils.isBlank(dto.getUsers())) {
+        //     throw new RuntimeException("适应人群为空");
+        // }
+        //
+        // if (StringUtils.isBlank(dto.getCharge())) {
+        //     throw new RuntimeException("收费规则为空");
+        // }
          //对数据进行封装，调用mapper进行数据持久化
         CourseBase courseBase = new CourseBase();
         //将传入dto的数据设置到 courseBase对象
